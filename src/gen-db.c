010ca48ae6bc232ed1729778c3982fe94242805d
MetaAmbience <camjh99@gmail.com>
2014-05-27 17:53:48 -0700

    Minor changes to variable names

--- src/gen-db.c
+++ src/gen-db.c
@@ -13,11 +13,11 @@
 #include "dialogue.h"
 #include "gui.h"
 
-#define LOAD_WIDTH      600 /**< Load window width. */
-#define LOAD_HEIGHT     500 /**< Load window height. */
+#define WND_WIDTH      600 
+#define WND_HEIGHT     500 
 
-#define BUTTON_WIDTH    80 /**< Button width. */
-#define BUTTON_HEIGHT   30 /**< Button height. */
+#define BUTTON_WIDTH    80 
+#define BUTTON_HEIGHT   30 
 
 void gendb_dbscreen_close(unsigned int wnd);
 
@@ -25,20 +25,20 @@ void gendb_dbscreen_close(unsigned int wnd);
  * @brief Opens the load game menu.
  */
 void gendb_loadDatabaseScreen (void){
-   unsigned int wid;
+   unsigned int wnd;
 
-   wid = window_create( "Planet/System Database (For Galaxy Generation)", -1, -1, LOAD_WIDTH, LOAD_HEIGHT );
+   wnd = window_create( "Planet/System Database (For Galaxy Generation)", -1, -1, WND_WIDTH, WND_HEIGHT );
 
-   window_addList( wid, 20, -50,
-         LOAD_WIDTH-40-BUTTON_WIDTH-20, LOAD_HEIGHT-70,
-         "lstSaves", NULL, 0, 0, NULL );
+   window_addList( wnd, 20, -50,
+         WND_WIDTH-40-BUTTON_WIDTH-20, WND_HEIGHT-70,
+         "lstPlanets", NULL, 0, 0, NULL );
 
-   window_addButton( wid, -20, 20, BUTTON_WIDTH, BUTTON_HEIGHT,
+   window_addButton( wnd, -20, 20, BUTTON_WIDTH, BUTTON_HEIGHT,
          "btnBack", "Back", gendb_dbscreen_close );
-   window_addButton( wid, -20, 20 + BUTTON_HEIGHT+20, BUTTON_WIDTH, BUTTON_HEIGHT,
-         "btnLoad", "Del", NULL );
-   window_addButton( wid, -20, 20 + 2 * (BUTTON_HEIGHT + 20), BUTTON_WIDTH, BUTTON_HEIGHT,
-         "btnDelete", "Add", NULL );
+   window_addButton( wnd, -20, 20 + BUTTON_HEIGHT+20, BUTTON_WIDTH, BUTTON_HEIGHT,
+         "btnDel", "Del", NULL );
+   window_addButton( wnd, -20, 20 + 2 * (BUTTON_HEIGHT + 20), BUTTON_WIDTH, BUTTON_HEIGHT,
+         "btnAdd", "Add", NULL );
 }
 
 void gendb_dbscreen_close(unsigned int wnd) {
